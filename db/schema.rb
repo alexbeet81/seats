c07c6c01d8e32d0040ad1e9ad5ae188645b3eab5
Alex Pritchard <alexbeet1234@gmail.com>
2021-09-02 21:28:57 +0800

    created seed file

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,39 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_02_131310) do
+ActiveRecord::Schema.define(version: 2021_09_02_132322) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "class_groups", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_class_groups_on_user_id"
+  end
+
+  create_table "seating_charts", force: :cascade do |t|
+    t.integer "number_of_students"
+    t.integer "rows"
+    t.integer "columns"
+    t.bigint "class_group_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["class_group_id"], name: "index_seating_charts_on_class_group_id"
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string "chinese_name"
+    t.string "english_name"
+    t.integer "number"
+    t.integer "participation_score"
+    t.bigint "class_group_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["class_group_id"], name: "index_students_on_class_group_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +56,7 @@ ActiveRecord::Schema.define(version: 2021_09_02_131310) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "class_groups", "users"
+  add_foreign_key "seating_charts", "class_groups"
+  add_foreign_key "students", "class_groups"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,26 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+puts "destroying old users"
+
+User.destroy_all
+
+puts "users destroyed"
+
+puts "creating new user"
+teacher = User.create!(email: 'alex@alex.com', password: 123456)
+
+puts "creating new class group"
+class_group = ClassGroup.create!(name: 'Class One', user_id: teacher.id)
+
+puts "creating new seating chart"
+seating_chart = SeatingChart.create!(number_of_students: 25, rows: 5, columns: 5, class_group_id: class_group.id)
+
+puts "creating new students"
+25.times do |i|
+  new_student = Student.create!(chinese_name: "我名字", english_name: "Student #{i}", number: i, participation_score: rand(10), class_group_id: glass_group.id)
+  puts "created new student #{i}"
+end
+
+puts "finished!"
