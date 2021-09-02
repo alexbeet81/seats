6dfbc621bf02eb29eb51733fad1952d384f34c37
Alex Pritchard <alexbeet1234@gmail.com>
2021-09-02 21:30:49 +0800

    dependant to dependent

--- app/models/class_group.rb
+++ app/models/class_group.rb
@@ -1,6 +1,6 @@
 class ClassGroup < ApplicationRecord
   belongs_to :user
 
-  has_many :seating_charts, dependant: :destroy
-  has_many :students, dependant: :destroy
+  has_many :seating_charts, dependent: :destroy
+  has_many :students, dependent: :destroy
 end
