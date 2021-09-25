9c4b1dec94fb21c2838ff3cdae1c4e72145ac9e0
Alex Pritchard <alexbeet1234@gmail.com>
2021-09-25 12:20:44 +0800

    Changed background color - testing to see if I had set up the css file correctly

--- app/javascript/components/ClassGroups/ClassGroups.js
+++ app/javascript/components/ClassGroups/ClassGroups.js
@@ -1,15 +1,31 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, Fragment } from 'react';
+import axios from 'axios';
+import './ClassGroups.css';
 
 const ClassGroups = () => {
   const [classGroups, setClassGroups] = useState([])
 
-  useEffects(() => {
+  useEffect(() => {
     // Get all of the class groups from api
     // update class groups in state
+
+    axios.get('/api/v1/class_groups.json')
+    .then( resp => {
+      setClassGroups(resp.data.data)
+    })
+    .catch( resp => console.log(resp))
+  }, [classGroups.length])
+
+  const list = classGroups.map( item => {
+    return (
+      <li key={item.attributes.name}>{item.attributes.name}</li>
+    )
   })
 
   return (
-    <div>This is ClassGroup#index page</div>
+    <div className="background">
+      <ul>{list}</ul>
+    </div>
   )
 }
 
